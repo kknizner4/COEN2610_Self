47fee02c5786a258064148ea7a00a80664a48728
kknizner4 <100310021+kknizner4@users.noreply.github.com>
2022-05-09 13:20:22 -0500

    Update final.js

--- final.js
+++ final.js
@@ -1,6 +1,5 @@
-// app.js - RESTful API server using http: 
-// @MarquetteU   F Frigo  06-Apr-2022
-//
+// final.js - RESTful API server using http: 
+// Katie Knizner 9 May 2022
 // To start server:  $ node app.js
 // For client with localhost use web browser:  http://localhost:3000 or
 // For remote host use http://bloomcounty.eng.mu.edu:3000
